9cb46c9a1bc58525b7312cd6b6c548fcd7110d40
straksi <kk095@yandex.ru>
2022-06-07 19:28:28 +0300

    next.config cganges

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['lh3.googleusercontent.com', 'cdn-icons-png.flaticon.com'],
+    domains: ['lh3.googleusercontent.com', 'cdn-icons-png.flaticon.com', '/_next/image', 'straksi-chat.vercel.app'],
   },
 }
 
